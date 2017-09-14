3880b734ace4c35f44417fac22689f8479ce17c4
Mark Loyman <Mark.Loyman@gmail.com>
2017-09-14 23:13:47 +0300

    support retrieval of all related annotations by 'info' and 'nod_ids' fields

--- LIDC/lidcUtils.py
+++ LIDC/lidcUtils.py
@@ -4,53 +4,106 @@ import matplotlib.pyplot as plt
 
 #plt.interactive(False)
 
-def getAnnotation(info, return_all=False):
+def getNoduleSize(nodule):
+    # take largest dimension over all annotations
+    # nodule: cluster of annotations
+    bb = 0
+    for ann in nodule:
+        bb = max(bb, max(ann.bbox_dimensions()))
+    return bb
+
+def getAnnotation(info, return_all=False, nodule_ids=None):
     qu = pl.query(pl.Scan).filter(pl.Scan.patient_id == info[0],
                                   pl.Scan.study_instance_uid == info[1],
                                   pl.Scan.series_instance_uid == info[2])
     assert (qu.count() == 1)
     scan = qu.first()
 
-    if (info[3] is not None) and (return_all is False):
-        nodule_id_list =  [ann._nodule_id for ann in scan.annotations]
-        if info[3] in nodule_id_list:
-            id =  nodule_id_list.index(info[3])
-            ann = scan.annotations[id]
-        else:
-            return None
-    else:
-        ann = scan.annotations
+    anns = []
 
-    return ann
+    if return_all is True:
+        if nodule_ids is None:
+            print('WRN: Returning ALL annotations from scan')
+            anns= scan.annotations
+
+        else:
+            nodule_id_list = [a._nodule_id for a in scan.annotations]
+            for nd_id in nodule_ids:
+                if nd_id in nodule_id_list:
+                    index = nodule_id_list.index(nd_id)
+                    anns.append(scan.annotations[index])
+                else:
+                    print("Nodule '{}' not found".format(nd_id))
+
+    else:
+    # return_all is False
+        if info[3] is None:
+            print('WRN: Returning ALL annotations from scan')
+            anns = scan.annotations
+        else:
+            nodule_id_list = [ann._nodule_id for ann in scan.annotations]
+            if info[3] in nodule_id_list:
+                id = nodule_id_list.index(info[3])
+                anns = scan.annotations[id]
+            else:
+                print("Nodule '{}' not found".format(info[3]))
+                anns = None
+
+    #if info[3] is not None:
+    #    if return_all:
+    #        ann_clusters = scan.cluster_annotations()
+    #        for clust in ann_clusters:
+    #            if info[3] in [ann._nodule_id for ann in clust]:
+    #               return clust
+    #        return None
+    #    else:
+    #        nodule_id_list = [ann._nodule_id for ann in scan.annotations]
+    #        if info[3] in nodule_id_list:
+    #            id = nodule_id_list.index(info[3])
+    #            ann = scan.annotations[id]
+    #        else:
+    #            return None
+    #else:
+    #    ann = scan.annotations
+
+    return anns
+
 
 def CheckPatch(entry):
     info = entry['info']
     print(info)
+
+    print('Patch Stats: Mean={}, Min={}, Max={}'.format( np.mean(entry['patch']),
+                                                         np.min(entry['patch']),
+                                                         np.max(entry['patch'])
+                                                    ))
     ann = getAnnotation(info)
-    ann.visualize_in_scan()
+
     plt.figure('Patch')
     plt.imshow(entry['patch']+1000*entry['mask'])
-    plt.title('Patch')
-    plt.show()
+    plt.title('Patch: {}'.format(np.mean(entry['rating'],axis=(0))))
 
+    ann.visualize_in_scan()
+
+    plt.show()
 
 def calc_rating(meta_data, method='mean'):
     if method is 'single':
         # currently only one of the ratings is taken into account
-        ann = getAnnotation(meta_data, return_all=False)
-        rating = ann.feature_vals()
+        ann     = getAnnotation(meta_data, return_all=False)
+        rating  = ann.feature_vals()
     elif method is 'malig':
-        ann = getAnnotation(meta_data)
-        rating = ann.feature_vals()[-1]
+        ann     = getAnnotation(meta_data)
+        rating  = ann.feature_vals()[-1]
         #rating = (rating - 3.0)/2
     elif method is 'mean':
         all_anns = getAnnotation(meta_data, return_all=True)
-        rating = np.mean([a.feature_vals() for a in all_anns], axis=(0))
+        rating   = np.mean([a.feature_vals() for a in all_anns], axis=(0))
     elif method is 'raw':
         all_anns = getAnnotation(meta_data, return_all=True)
-        rating = [a.feature_vals() for a in all_anns]
+        rating   = [a.feature_vals() for a in all_anns]
     elif method is 'confidence':
         all_anns = getAnnotation(meta_data, return_all=True)
-        rating = np.std([a.feature_vals() for a in all_anns], axis=(0))
+        rating   = np.std([a.feature_vals() for a in all_anns], axis=(0))
 
     return np.array(rating)

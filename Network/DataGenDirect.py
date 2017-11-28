a0d1ce5e39bfad0eac04489507e9428d9ea3baaa
Mark Loyman <Mark.Loyman@gmail.com>
2017-11-28 19:32:21 +0200

    DataGen for directArch

---
+++ Network/DataGenDirect.py
@@ -0,0 +1,149 @@
+import numpy as np
+
+try:
+    from Network.data import load_nodule_dataset, prepare_data
+    from Network.dataUtils import augment, crop_center, get_sample_weight, get_class_weight
+except:
+    from data import load_nodule_dataset, prepare_data
+    from dataUtils import augment, crop_center, get_sample_weight, get_class_weight
+
+class DataGeneratorDir(object):
+    """docstring for DataGenerator"""
+
+    def __init__(self,  data_size= 128, model_size=128, res='Legacy', sample='Normal', batch_sz=32,
+                        do_augment=False, augment=None, use_class_weight=False, class_weight='dummy', debug=False,
+                        val_factor = 1, balanced=False, group_by_size=False):
+
+        dataset = load_nodule_dataset(size=data_size, res=res, sample=sample, apply_mask_to_patch=debug)
+
+        self.train_set = dataset[2]
+        self.valid_set = dataset[1]
+
+        self.batch_sz = batch_sz
+        self.data_size  = data_size
+        self.model_size = model_size
+
+        self.val_factor = val_factor
+        if balanced:
+            self.trainN = 666 // self.batch_sz
+
+        else:
+            self.trainN = 1023 // self.batch_sz
+        self.valN = val_factor * (339 // self.batch_sz)
+
+        self.balanced = balanced
+
+        self.use_class_weight = use_class_weight
+        self.class_weight_method = class_weight
+        self.do_augment = do_augment
+        self.augment = augment
+        if do_augment: assert(augment is not None)
+
+        print("Trainings Sets: {}, Validation Sets: {}".format(self.trainN, self.valN))
+
+    def augment_all(self, images, masks):
+        images = [augment(im, msk,
+                          size=self.model_size,
+                          max_angle=self.augment['max_angle'],
+                          flip_ratio=self.augment['flip_ratio'],
+                          crop_stdev=self.augment['crop_stdev'])[0]
+                  for im, msk in zip(images, masks)]
+        return np.array(images)
+
+    def select_balanced(self, some_set, labels, N, permutation):
+        b = some_set[0 == labels][:N]
+        m = some_set[1 == labels][:N]
+        merged = np.concatenate([b, m], axis=0)
+        reshuff = merged[permutation]
+        return reshuff
+
+    def next(self, set, is_training=False):
+        verbose = 1
+        epoch = 0
+        while 1:
+            print('Run Gen {}: {}'.format(epoch, np.where(is_training, 'Training', 'Validation')))
+            size = self.data_size if self.do_augment else self.model_size
+            #images, labels, masks, confidence = \
+            images, labels, masks = \
+                prepare_data(set, classes=2, verbose=verbose, reshuffle=True)
+            Nb = np.count_nonzero(1-np.argmax(labels, axis=1))
+            Nm = np.count_nonzero(np.argmax(labels, axis=1))
+            N = np.minimum(Nb, Nm)
+            if verbose:
+                print("Benign: {}, Malignant: {}".format(Nb, Nm))
+            if self.balanced and is_training:
+                new_order = np.random.permutation(2*N)
+                labels_ = np.argmax(labels, axis=1)
+                images = self.select_balanced(images, labels_, N, new_order)
+                labels = self.select_balanced(labels, labels_, N, new_order)
+                masks = self.select_balanced(masks, labels_, N, new_order)
+                if verbose:
+                    Nb = np.count_nonzero(1 - np.argmax(labels, axis=1))
+                    Nm = np.count_nonzero(np.argmax(labels, axis=1))
+                    print("Balanced - Benign: {}, Malignant: {}".format(Nb, Nm))
+            if self.do_augment and is_training and (epoch >= self.augment['epoch']):
+                    if epoch == self.augment['epoch']:
+                        print("Begin augmenting")
+                    images = self.augment_all(images)
+            else:
+                images = np.array([crop_center(im, msk, size=self.model_size)[0]
+                                    for im, msk in zip(images, masks)])
+            if verbose:
+                print("images after augment/crop: {}".format(images[0].shape))
+
+            if self.use_class_weight:
+                class_weight = get_class_weight(confidence, method=self.class_weight_method)
+
+            # split into batches
+            split_idx = [b for b in range(self.batch_sz, images.shape[0], self.batch_sz)]
+            images = np.array_split(images, split_idx)
+            labels = np.array_split(labels,  split_idx)
+            masks  = np.array_split(masks, split_idx)
+            #confidence = np.array_split(confidence,  split_idx)
+
+            if verbose == 1: print("batch size:{}, sets:{}".format(images[0].shape[0], len(images[0])))
+
+            # if last batch smaller than batch_sz, discard it
+            if images[-1].shape[0] < self.batch_sz:
+                images = images[:-1]
+                labels = labels[:-1]
+                masks  = masks[:-1]
+                #if self.use_class_weight:
+                #    confidence = confidence[:-1]
+                if verbose == 1:
+                    print("discard last unfull batch -> sets:{}".format(len(images)))
+
+            if epoch == 0:
+                if is_training:
+                    assert(len(images) == self.trainN)
+                else:
+                    assert( (self.val_factor*len(images)) == self.valN)
+
+            #for im, lbl, msk, cnf in zip(images, labels, masks, confidence):
+            for im, lbl, msk in zip(images, labels, masks):
+                if self.use_class_weight:
+                    assert(False)
+                    w = get_sample_weight(cnf,  wD=class_weight['D'],
+                                                wSB=class_weight['SB'],
+                                                wSM=class_weight['SM']
+                                          )
+                    if verbose == 1:
+                        print([(li, np.round(10*wi, 2).astype('uint')) for li, wi in zip(lbl, w)])
+                    verbose = 0
+                    yield (im, lbl, w)
+                else:
+                    yield (im, lbl)
+            epoch = epoch + 1
+            verbose = 0
+
+    def next_train(self):
+        return self.next(self.train_set, is_training=True)
+
+    def next_val(self):
+        return self.next(self.valid_set, is_training=False)
+
+    def train_N(self):
+        return self.trainN
+
+    def val_N(self):
+        return self.valN
--- Network/directArch.py
+++ Network/directArch.py
@@ -7,69 +7,99 @@ from keras.layers import Dense
 from keras.layers import Input
 from keras.models import Model
 from keras.optimizers import Adam
+from keras.metrics import categorical_accuracy
 
-from Analysis.analysis import history_summarize
-from Network.dataUtils import get_class_weight
-from Network.modelUtils import sensitivity, specificity, precision
+try:
+    from Analysis.analysis import history_summarize
+    from Network.dataUtils import get_class_weight
+    from Network.modelUtils import sensitivity, f1, precision, specificity
+    output_dir = './output'
+except:
+    from dataUtils import get_class_weight
+    from modelUtils import sensitivity, f1, precision, specificity
+    output_dir = '/output'
 
 
 class directArch:
 
-    def __init__(self, model_loader, input_shape, classes=2):
+    def __init__(self, model_loader, input_shape, classes=2, pooling='rmac', output_size=1024, normalize=False):
     #   input_shape of form: (size, size,1)
 
         img_input   = Input(shape=input_shape)
-        self.base   = model_loader(input_tensor=img_input, input_shape=input_shape, pooling='rmac')
-        pred_layer  = Dense(classes, activation='softmax', name='predictions')(self.base)
+        self.base   = model_loader(input_tensor=None, input_shape=input_shape, return_model=True,
+                                  pooling=pooling, output_size=output_size, normalize=normalize)
+        pred_layer  = Dense(output_size, activation='relu')(self.base(img_input))
+        pred_layer  = Dense(classes, activation='softmax', name='predictions')(pred_layer)
 
         self.model = Model(img_input, pred_layer, name='directArch')
         self.data_ready  = False
         self.model_ready = False
 
-    def compile(self, learning_rate):
-        self.model.compile( optimizer   = Adam(lr=learning_rate, decay=0.01*learning_rate),
-                            loss        = 'categorical_crossentropy',
-                            metrics     = ['categorical_accuracy', sensitivity, specificity, precision] )
+    def compile(self, learning_rate = 0.001, decay=0.1):
+        categorical_accuracy.__name__ = 'accuracy'
+        sensitivity.__name__ = 'recall'
+        self.model.compile( optimizer   = Adam(lr=learning_rate, decay=decay),
+                            loss        = 'categorical_crossentropy', #'categorical_crossentropy', mean_squared_error
+                            metrics     = [categorical_accuracy, f1, sensitivity, precision, specificity] )
         self.model_ready = True
 
-    def load_data(self, images_train, labels_train, images_valid, labels_valid):
+    def load_data(self, images_train, labels_train, images_valid, labels_valid, batch_sz=32):
         self.images_train = images_train
         self.labels_train = labels_train
         self.images_valid = images_valid
         self.labels_valid = labels_valid
-
+        self.batch_sz = batch_sz
         self.data_ready = True
 
-    def train(self, label=''):
+    def load_generator(self, data_gen):
+        self.data_gen = data_gen
 
-        model_checkpoint = ModelCheckpoint('w_'+label+'_{epoch:02d}-{categorical_accuracy:.2f}-{val_categorical_accuracy:.2f}.h5',
-                                           monitor='val_loss', save_best_only=True)
+    def train(self, label='', epoch=0, n_epoch=100, gen = False):
 
-        on_plateau       = ReduceLROnPlateau(monitor='val_loss', factor=0.5, epsilon=0.05, patience=10, min_lr=1e-8, verbose=1)
+        checkpoint = ModelCheckpoint(output_dir+'/Weights/w_' + label + '_{epoch:02d}-{accuracy:.2f}-{val_accuracy:.2f}.h5',
+                                           monitor='val_loss', save_best_only=False)
+        #on_plateau       = ReduceLROnPlateau(monitor='val_loss', factor=0.5, epsilon=0.05, patience=10, min_lr=1e-8, verbose=1)
         early_stop       = EarlyStopping(monitor='loss', min_delta=0.05, patience=15)
-        board            = TensorBoard(log_dir='./logs', histogram_freq=1)
-
-        class_weight     = get_class_weight(self.labels_train)
-
+        if gen:
+            board            = TensorBoard(log_dir=output_dir+'/logs', histogram_freq=0, write_graph=False)
+        else:
+            board = TensorBoard(log_dir=output_dir + '/logs', histogram_freq=1, write_graph=True,
+                                write_images=True, batch_size=64, write_grads=True)
+                                #embeddings_freq=5, embeddings_layer_names='n_embedding', embeddings_metadata='meta.tsv')
         start = timer()
         total_time = None
         try:
-            history = self.model.fit(
-                x = self.images_train,
-                y = self.labels_train,
-                shuffle = True,
-                class_weight = class_weight,
-                validation_data = (self.images_valid, self.labels_valid),
-                batch_size = 32,
-                epochs = 100,
-                callbacks = [model_checkpoint, on_plateau, early_stop, board],
-                verbose = 2
-            )
-
+            if gen:
+                print("Train Steps: {}, Val Steps: {}".format(self.data_gen.train_N(), self.data_gen.val_N()))
+                history = self.model.fit_generator(
+                    generator =  self.data_gen.next_train(),
+                    steps_per_epoch= self.data_gen.train_N(),
+                    #class_weight = class_weight,
+                    validation_data  = self.data_gen.next_val(),
+                    validation_steps = self.data_gen.val_N(),
+                    initial_epoch = epoch,
+                    epochs = epoch+n_epoch,
+                    max_queue_size=3,
+                    callbacks = [checkpoint, board], # early_stop, on_plateau, early_stop, checkpoint_val, lr_decay, pb
+                    verbose = 2
+                )
+            else:
+                class_weight = get_class_weight(self.labels_train)
+                history = self.model.fit(
+                    x = self.images_train,
+                    y = self.labels_train,
+                    shuffle = True,
+                    #class_weight = class_weight,
+                    validation_data = (self.images_valid, self.labels_valid),
+                    batch_size = self.batch_sz,
+                    initial_epoch=epoch,
+                    epochs = epoch+n_epoch,
+                    callbacks = [checkpoint, early_stop, board],
+                    verbose = 2
+                )
             total_time = (timer() - start) / 60 / 60
-
-            history_summarize(history, label)
-            pickle.dump(history.history, open('history-{}.p'.format(label), 'bw'))
+            # history_summarize(history, label)
+            pickle.dump(history.history, open(output_dir+ '/history/history-{}.p'.format(label), 'bw'))
 
         finally:
             if total_time is None:

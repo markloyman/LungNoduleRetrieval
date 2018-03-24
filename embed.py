07a6f3a1900679b760d52d9226a8a9637cd6d176
Mark Loyman <Mark.Loyman@gmail.com>
2018-03-24 12:49:57 +0300

    embed all datasets (train, valid and test)

--- embed.py
+++ embed.py
@@ -5,28 +5,27 @@ from Network import embed
 ## ======= Setup ======= ##
 ## ===================== ##
 
-network = 'dirR'
+network = 'trip'
 
-wRuns = ['010']
-wEpchs= [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]
+wRuns = ['027']
+wEpchs= [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
 
 pooling = 'rmac'
 
 # 0     Test
 # 1     Validation
 # 2     Training
-DataSubSet = 2
+DataSubSet = 0
 
-if   DataSubSet == 0:
-    post = "Test"
-elif DataSubSet == 1:
-    post = "Valid"
-elif DataSubSet == 2:
-    post = "Train"
-else:
-    assert False
-print("{} Set Analysis".format(post))
-print('='*15)
+def data_label(idx):
+    if   idx == 0:
+        return "Test"
+    elif idx == 1:
+        return "Valid"
+    elif idx == 2:
+        return "Train"
+    else:
+        assert False
 
 ## ========================= ##
 ## ======= Evaluate  ======= ##
@@ -35,8 +34,16 @@ print('='*15)
 start = timer()
 try:
 
-    Embd = embed.Embeder(network, pooling=pooling)
-    Embd.run(runs=wRuns, post=post, data_subset_id=DataSubSet, epochs=wEpchs)
+    Embd = embed.Embeder(network, pooling=pooling, categorize=True)
+
+    DataSubSet = 0
+    Embd.run(runs=wRuns, post=data_label(DataSubSet), data_subset_id=DataSubSet, epochs=wEpchs)
+
+    DataSubSet = 1
+    Embd.run(runs=wRuns, post=data_label(DataSubSet), data_subset_id=DataSubSet, epochs=wEpchs)
+
+    DataSubSet = 2
+    Embd.run(runs=wRuns, post=data_label(DataSubSet), data_subset_id=DataSubSet, epochs=wEpchs)
 
 finally:
     plt.show()

030d2c49e917b92530bb840af4d29861768866f4
Mark Loyman <Mark.Loyman@gmail.com>
2018-04-07 23:09:42 +0300

    - predict malignancy

--- predict.py
+++ predict.py
@@ -1,16 +1,17 @@
-from init import *
+from Network.predict import Malignancy as PredictMal
 from Network.predict import Rating as PredictRating
 
-run = '011X'
-epochs = [5, 15, 25, 35, 45, 55, 65, 75, 85, 95]
+run = '201'
+conf = 4
+epochs = [40]
 
-pooling = 'rmac'
+pooling = 'max'
 rating_scale = 'none'
 
 # 0     Test
 # 1     Validation
 # 2     Training
-DataSubSet = 2
+DataSubSet = 0
 
 if DataSubSet == 0:
     post = "Test"
@@ -26,13 +27,28 @@ print('=' * 15)
 start = timer()
 try:
 
-    PredRating = PredictRating(pooling=pooling)
-    for e in epochs:
-        WeightsFile = FileManager.Weights('dirR').name(run, epoch=e)
-        PredFile = FileManager.Pred(type='rating', pre='dirR')
-        out_file = PredFile(run=run, epoch=e, dset=post)
+    if conf is not None:
+        run += 'c{}'.format(conf)
 
-        PredRating.predict_rating(WeightsFile, out_file, DataSubSet, rating_scale=rating_scale)
+    if False:
+        PredRating = PredictRating(pooling=pooling)
+        for e in epochs:
+            WeightsFile = FileManager.Weights('dirR').name(run, epoch=e)
+            PredFile = FileManager.Pred(type='rating', pre='dirR')
+            out_file = PredFile(run=run, epoch=e, dset=post)
+
+            PredRating.predict_rating(WeightsFile, out_file, DataSubSet, rating_scale=rating_scale)
+    else:
+        PredMal = PredictMal(pooling=pooling)
+        for e in epochs:
+            WeightsFile = FileManager.Weights('dir').name(run, epoch=e)
+            print("weight: {}".format(WeightsFile))
+            PredFile = FileManager.Pred(type='malig', pre='dir')
+            out_file = PredFile(run=run[:-1] + str(conf), epoch=e, dset=post)
+
+            PredMal.data_size = 128
+            PredMal.res = 0.5
+            PredMal.predict_malignancy(WeightsFile, out_file, DataSubSet, configuration=conf)
 
     print('Ready...')
 

1145c2eebe8563cb0bc3099394232c90e247f92f
Mark Loyman <Mark.Loyman@gmail.com>
2018-05-19 00:04:55 +0300

    rating predict refactor

--- Network/predict.py
+++ Network/predict.py
@@ -13,58 +13,59 @@ class Rating:
         self.pred_file_format = '.\output\embed\predR_dirR{}_E{}_{}.p'
         self.pooling = pooling
 
+        # Setup
+        self.in_size = 128
+        self.out_size = 128
+        input_shape = (self.in_size, self.in_size, 1)
+
+        # prepare model
+        self.model = directArch(miniXception_loader, input_shape, objective="rating", pooling=self.pooling, output_size=self.out_size,
+                           normalize=True)
+
+    def load_dataset(self, data_subset_id, size=160, sample='Normal', res=0.5, rating_scale='none', configuration=None):
+        # prepare test data
+        self.images_test, self.labels_test, self.classes_test, self.masks_test, self.meta_test = \
+            prepare_data_direct(load_nodule_dataset(configuration=configuration, size=size, res=res, sample=sample)[data_subset_id],
+                                size=size,
+                                return_meta=True, objective="rating", rating_scale=rating_scale, verbose=1,
+                                balanced=False)
+
+        print("Data ready: images({}), labels({})".format(self.images_test[0].shape, self.labels_test.shape))
+        print("Range = [{:.2f},{:.2f}]".format(np.min(self.images_test[0]), np.max(self.images_test[0])))
+
+        self.images_test = np.array([crop_center(im, msk, size=self.in_size)[0]
+                                for im, msk in zip(self.images_test, self.masks_test)])
+
+        print("Image size changed to {}".format(self.images_test.shape))
+        print('Mask not updated')
+
     def pred_filename(self, run, epoch, post):
         return self.pred_file_format.format(run, epoch, post)
 
     def load(self, run, epoch, post):
         filename = self.pred_filename(run=run, epoch=epoch, post=post)
-        images, predict, meta_data, labels, masks = pickle.load(open(filename, 'br'))
-        return images, predict, meta_data, labels, masks
+        predict, epochs, images, meta_data, labels, masks = pickle.load(open(filename, 'br'))
+        return predict, epochs, images, meta_data, labels, masks
 
-    def predict_rating(self, weights_file, out_filename, data_subset_id, rating_scale='none'):
+    def predict_rating(self, weights_file, out_filename):
 
-        # Setup
-        size = 144
-        input_shape = (128, 128, 1)
-        sample = 'Normal'
-        res = '0.5I'
-        in_size = 128
-        out_size = 128
-
-        # prepare model
-        model = directArch(miniXception_loader, input_shape, objective="rating", pooling=self.pooling, output_size=out_size,
-                           normalize=True)
         if weights_file is not None:
-            model.load_weights(weights_file)
+            self.model.load_weights(weights_file)
             print('Load from: {}'.format(weights_file))
         else:
             print('Model without weights')
 
-        # prepare test data
-        images_test, labels_test, classes_test, masks_test, meta_test = \
-            prepare_data_direct(load_nodule_dataset(size=size, res=res, sample=sample)[data_subset_id], size=size,
-                                return_meta=True, objective="rating", rating_scale=rating_scale, verbose=1, balanced=False)
-
-        print("Data ready: images({}), labels({})".format(images_test[0].shape, labels_test.shape))
-        print("Range = [{:.2f},{:.2f}]".format(np.min(images_test[0]), np.max(images_test[0])))
-
-        images_test = np.array([crop_center(im, msk, size=in_size)[0]
-                                 for im, msk in zip(images_test, masks_test)])
-
-        print("Image size changed to {}".format(images_test.shape))
-        print('Mask not updated')
-
         # eval
         print("Begin Predicting...")
-        pred = model.predict(images_test, round=False)
+        pred = self.model.predict(self.images_test, round=False)
         print("Predication Ready")
         print("\tshape = {}\n\trange [{}, {}]".format(pred.shape, np.min(pred), np.max(pred)))
 
 
-        pickle.dump((images_test, pred, meta_test, labels_test, masks_test), open(out_filename, 'bw'))
-        print("Saved to: {}".format(out_filename))
+        #pickle.dump((images_test, pred, meta_test, labels_test, masks_test), open(out_filename, 'bw'))
+        #print("Saved to: {}".format(out_filename))
 
-        return (images_test, pred, meta_test, labels_test, masks_test), out_filename
+        return (self.images_test, pred, self.meta_test, self.classes_test, self.labels_test, self.masks_test), out_filename
 
 
 
--- predict.py
+++ predict.py
@@ -1,9 +1,12 @@
 from Network.predict import Malignancy as PredictMal
 from Network.predict import Rating as PredictRating
+from init import *
+from Network import FileManager
 
-run = '201'
-conf = 4
-epochs = [40]
+
+run = '200'
+conf = None
+epochs = np.arange(1, 100)
 
 pooling = 'max'
 rating_scale = 'none'
@@ -11,7 +14,7 @@ rating_scale = 'none'
 # 0     Test
 # 1     Validation
 # 2     Training
-DataSubSet = 0
+DataSubSet = 1
 
 if DataSubSet == 0:
     post = "Test"
@@ -30,21 +33,28 @@ try:
     if conf is not None:
         run += 'c{}'.format(conf)
 
-    if False:
+    if True:
         PredRating = PredictRating(pooling=pooling)
+        PredRating.load_dataset(data_subset_id=DataSubSet, size=128, rating_scale=rating_scale, configuration=conf)
+        preds = []
+        valid_epochs = []
         for e in epochs:
             WeightsFile = FileManager.Weights('dirR').name(run, epoch=e)
             PredFile = FileManager.Pred(type='rating', pre='dirR')
-            out_file = PredFile(run=run, epoch=e, dset=post)
+            out_file = PredFile(run=run, dset=post)
 
-            PredRating.predict_rating(WeightsFile, out_file, DataSubSet, rating_scale=rating_scale)
+            data, out_filename = PredRating.predict_rating(WeightsFile, out_file)
+            images_test, pred, meta_test, classes_test, labels_test, masks_test = data
+            preds.append(np.expand_dims(pred, axis=0))
+        preds = np.concatenate(preds, axis=0)
+        pickle.dump((preds, np.array(epochs), images_test, meta_test, classes_test, labels_test, masks_test), open(out_filename, 'bw'))
     else:
         PredMal = PredictMal(pooling=pooling)
         for e in epochs:
             WeightsFile = FileManager.Weights('dir').name(run, epoch=e)
             print("weight: {}".format(WeightsFile))
             PredFile = FileManager.Pred(type='malig', pre='dir')
-            out_file = PredFile(run=run[:-1] + str(conf), epoch=e, dset=post)
+            out_file = PredFile(run=run[:-1] + str(conf), dset=post)
 
             PredMal.data_size = 128
             PredMal.res = 0.5

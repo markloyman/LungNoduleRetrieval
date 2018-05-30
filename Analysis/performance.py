f4aabc1e04a0768042fbe5db5a49e9e09ac5c720
Mark Loyman <Mark.Loyman@gmail.com>
2018-05-30 09:26:38 +0300

    mean_cross_validated_index

--- Analysis/performance.py
+++ Analysis/performance.py
@@ -17,27 +17,54 @@ def precision(query, nn):
     assert(False)
 
 
-def eval_classification(run, net_type, metric, epochs, dset, NN=[3, 5, 7, 11, 17], cross_validation=False, n_groups=5):
-    Embed = FileManager.Embed(net_type)
-    Pred_L1O = []
+def merge_epochs(valid_epochs, min_element=4):
+    combined_epochs = [i for i, c in enumerate(np.bincount(np.concatenate(valid_epochs))) if c >= min_element]
+    return combined_epochs
 
+
+def mean_cross_validated_index(index, valid_epochs, combined_epochs):
+    merged = np.zeros((len(combined_epochs)))
+    merged_std = np.zeros((len(combined_epochs)))
+    for ep_id, epoch in enumerate(combined_epochs):
+        #collect = [[np.array(idx)[:, i] for i, e in enumerate(ve) if e == epoch] for idx, ve in zip(index, valid_epochs) if epoch in ve]
+        #collect = [np.array(idx)[np.argwhere(ve==epoch)[0]] for idx, ve in zip(index, valid_epochs) if epoch in ve]
+        collect = []
+        for idx, ve in zip(index, valid_epochs):
+            if epoch in ve:
+                for i, e in enumerate(ve):
+                    if e == epoch:
+                        collect += [idx[i, :]]
+        merged[ep_id] = np.mean(np.mean(collect, axis=-1), axis=0)
+        merged_std[ep_id] = np.mean(np.std(collect, axis=-1), axis=0)
+    return merged, merged_std
+
+
+def eval_classification(run, net_type, metric, epochs, dset, NN=[7, 11, 17], cross_validation=False, n_groups=5):
+    Embed = FileManager.Embed(net_type)
+    Pred_L1O = [[] for i in range(n_groups)]
+    valid_epochs = [[] for i in range(n_groups)]
     if cross_validation:
         # Load
         embed_source= [Embed(run + 'c{}'.format(c), dset) for c in range(n_groups)]
         Ret = Retriever(title='{}-{}'.format(net_type, run), dset=dset)
-        Ret.load_embedding(embed_source, multi_epcch=True)
-        valid_epochs = []
-        for E in epochs:
-            # Calc
-            pred_l1o = []
-            try:
-                for N in NN:
-                    pred_l1o.append(Ret.classify_kfold(epoch=E, n=N, k_fold=10, metric=metric))
-                Pred_L1O.append(np.array(pred_l1o))
-                valid_epochs.append(E)
-            except:
-                print("Epoch {} - no calculated embedding".format(E))
-        Pred_L1O = np.array(Pred_L1O)
+        for i, source in enumerate(embed_source):
+            Ret.load_embedding([source], multi_epcch=True)
+            for E in epochs:
+                # Calc
+                pred_l1o = []
+                try:
+                    for N in NN:
+                        pred_l1o.append(Ret.classify_kfold(epoch=E, n=N, k_fold=10, metric=metric))
+                    Pred_L1O[i].append(np.array(pred_l1o))
+                    valid_epochs[i].append(E)
+                except:
+                    print("Epoch {} - no calculated embedding".format(E))
+            Pred_L1O[i] = np.array(Pred_L1O[i])
+            valid_epochs[i] = np.array(valid_epochs[i])
+
+        combined_epochs = merge_epochs(valid_epochs)
+        P, P_std = mean_cross_validated_index(Pred_L1O, valid_epochs, combined_epochs)
+
     else:
         for E in epochs:
             # Load
@@ -49,37 +76,51 @@ def eval_classification(run, net_type, metric, epochs, dset, NN=[3, 5, 7, 11, 17
             for N in NN:
                 pred_l1o.append(Ret.classify_leave1out(n=N, metric=metric)[1])
             Pred_L1O.append(np.array(pred_l1o))
-        Pred_L1O = np.array(Pred_L1O)
+            P, P_std = np.mean(Pred_L1O, axis=-1), np.std(Pred_L1O, axis=-1)
 
-    return np.mean(Pred_L1O, axis=-1), np.std(Pred_L1O, axis=-1), valid_epochs
+    return P, P_std, combined_epochs
 
 
-def eval_retrieval(run, net_type, metric, epochs, dset, NN=[3, 5, 7, 11, 17], cross_validation=False, n_groups=5):
+def eval_retrieval(run, net_type, metric, epochs, dset, NN=[7, 11, 17], cross_validation=False, n_groups=5):
     Embed = FileManager.Embed(net_type)
-    Prec, Prec_b, Prec_m = [], [], []
-
+    Prec, Prec_b, Prec_m = [[] for i in range(n_groups)], [[] for i in range(n_groups)], [[] for i in range(n_groups)]
+    valid_epochs = [[] for i in range(n_groups)]
     if cross_validation:
         # Load
-        embed_source= [Embed(run + 'c{}'.format(c), dset) for c in range(n_groups)]
+        embed_source = [Embed(run + 'c{}'.format(c), dset) for c in range(n_groups)]
         Ret = Retriever(title='{}-{}'.format(net_type, run), dset=dset)
-        Ret.load_embedding(embed_source, multi_epcch=True)
-        valid_epochs = []
-        for E in epochs:
-            # Calc
-            prec, prec_b, prec_m = [], [], []
-            try:
-                Ret.fit(np.max(NN), metric=metric, epoch=E)
+        for i, source in enumerate(embed_source):
+            Ret.load_embedding(source, multi_epcch=True)
+            for E in epochs:
+                # Calc
+                prec, prec_b, prec_m = [], [], []
+                try:
+                    Ret.fit(np.max(NN), metric=metric, epoch=E)
+                except:
+                    print("Epoch {} - no calculated embedding".format(E))
+                    continue
                 for N in NN:
                     p, pb, pm = Ret.evaluate_precision(n=N)
                     prec.append(p)
                     prec_b.append(pb)
                     prec_m.append(pm)
-                Prec.append(np.array(prec))
-                Prec_b.append(np.array(prec_b))
-                Prec_m.append(np.array(prec_m))
-                valid_epochs.append(E)
-            except:
-                print("Epoch {} - no calculated embedding".format(E))
+                Prec[i].append(np.array(prec))
+                Prec_b[i].append(np.array(prec_b))
+                Prec_m[i].append(np.array(prec_m))
+                valid_epochs[i].append(E)
+
+            Prec[i] = np.array(Prec[i])
+            Prec_b[i] = np.array(Prec_b[i])
+            Prec_m[i] = np.array(Prec_m[i])
+            valid_epochs[i] = np.array(valid_epochs[i])
+
+        combined_epochs = epochs  # merge_epochs(valid_epochs)
+        P, P_std = mean_cross_validated_index(Prec, valid_epochs, combined_epochs)
+        #P, P_std = np.mean(np.mean(Prec, axis=-1), axis=0), np.mean(np.std(Prec, axis=-1), axis=0)
+        combined = 2 * np.array(Prec_b) * np.array(Prec_m) / (np.array(Prec_b) + np.array(Prec_m))
+        #F1, F1_std = np.mean(np.mean(combined, axis=-1), axis=0), np.mean(np.std(combined, axis=-1), axis=0)
+        F1, F1_std = mean_cross_validated_index(combined, valid_epochs, combined_epochs)
+
     else:
         for E in epochs:
             Ret = Retriever(title='', dset='')
@@ -100,13 +141,14 @@ def eval_retrieval(run, net_type, metric, epochs, dset, NN=[3, 5, 7, 11, 17], cr
             Prec_b.append(np.array(prec_b))
             Prec_m.append(np.array(prec_m))
 
-    # Pred_L1O = np.transpose(np.array(Pred_L1O))
-    Prec = (np.array(Prec))
-    Prec_m = (np.array(Prec_m))
-    Prec_b = (np.array(Prec_b))
-    f1 = 2 * Prec_b * Prec_m / (Prec_b + Prec_m)
+        Prec = np.array(Prec)
+        Prec_m = np.array(Prec_m)
+        Prec_b = np.array(Prec_b)
+        f1 = 2 * Prec_b * Prec_m / (Prec_b + Prec_m)
+        P, P_std = np.mean(Prec, axis=-1), np.std(Prec, axis=-1)
+        F1, F1_std = np.mean(f1, axis=-1), np.std(f1, axis=-1)
 
-    return np.mean(Prec, axis=-1), np.std(Prec, axis=-1), np.mean(f1, axis=-1), np.std(f1, axis=-1), valid_epochs
+    return P, P_std, F1, F1_std, valid_epochs
 
 
 def eval_correlation(run, net_type, metric, epochs, dset, rating_norm='none', cross_validation=False, n_groups=5):
--- eval_metric_space.py
+++ eval_metric_space.py
@@ -5,29 +5,48 @@ from Analysis.retrieval import Retriever
 from Network import FileManager
 from experiments import load_experiments
 from scipy.signal import savgol_filter
+
+# Setup
+
+experiment_name = 'Pooling'
 dset = 'Valid'
 rating_normalizaion = 'Scale' # 'None', 'Normal', 'Scale'
 metrics = ['l2']
 n_groups = 5
 
 
-runs, run_net_types, run_metrics, run_epochs, run_names = load_experiments('DirRating')
+runs, run_net_types, run_metrics, run_epochs, run_names, _, _ = load_experiments(experiment_name)
 
 # initialize figures
 
-plt.figure("Metric Space - {}".format(dset))
+plt.figure("{} Metric Space: {}".format(experiment_name, dset))
 p = [None] * len(metrics) * 5
 for i in range(5 * len(metrics)):
     p[i] = plt.subplot(len(metrics), 5, i + 1)
 
 # evaluate
 
+
+def mean_cross_validated_index(index, valid_epochs, combined_epochs):
+    merged = np.zeros((index[0].shape[0], len(combined_epochs)))
+    for ep_id, epoch in enumerate(combined_epochs):
+        #collect = [[idx[:, i] for i, e in enumerate(ve) if e == epoch] for idx, ve in zip(index, valid_epochs) if epoch in ve]
+        collect = []
+        for idx, ve in zip(index, valid_epochs):
+            if epoch in ve:
+                for i, e in enumerate(ve):
+                    if e == epoch:
+                        collect += [idx[:, i]]
+        merged[:, ep_id] = np.mean(collect, axis=0)
+    return merged
+
+
 Epochs, Idx_hubness, Idx_symmetry, Idx_concentration, Idx_contrast, Idx_kummar = [], [], [], [], [], []
 
-for m, metric in enumerate(metrics):
+for m, metric_ in enumerate(metrics):
     print("Begin: {} metric".format(metric))
-    for run, net_type, r, epochs in zip(runs, run_net_types, range(len(runs)), run_epochs):
-        plot_data_filename = './Plots/metric-space_{}{}.p'.format(net_type, run)
+    for run, net_type, r, epochs, metric in zip(runs, run_net_types, range(len(runs)), run_epochs, run_metrics):
+        plot_data_filename = './Plots/Data/metric-space_{}{}.p'.format(net_type, run)
         try:
             valid_epochs, idx_hubness, idx_symmetry, idx_concentration, idx_contrast, idx_kummar = pickle.load(open(plot_data_filename, 'br'))
             print("Loaded results for {}{}".format(net_type, run))
@@ -36,34 +55,52 @@ for m, metric in enumerate(metrics):
             # init
             Embed = FileManager.Embed(net_type)
             embed_source = [Embed(run + 'c{}'.format(c), dset) for c in range(n_groups)]
-            idx_hubness, idx_symmetry, idx_concentration, idx_contrast, idx_kummar, valid_epochs = [], [], [], [], [], []
+            idx_hubness, idx_symmetry, idx_concentration, idx_contrast, idx_kummar \
+                = [[] for i in range(n_groups)], [[] for i in range(n_groups)], [[] for i in range(n_groups)], [[] for i in range(n_groups)], [[] for i in range(n_groups)]
+            valid_epochs = [[] for i in range(n_groups)]
             # calculate
             Ret = Retriever(title='{}'.format(run), dset=dset)
-            embd, epoch_mask = Ret.load_embedding(embed_source, multi_epcch=True)
-            for e in epochs:
-                try:
-                    Ret.fit(metric=metric, epoch=e)
-                    indices, distances = Ret.ret_nbrs()
-                    # hubness
-                    idx_hubness.append(index.calc_hubness(indices))
-                    #   symmetry
-                    idx_symmetry.append(index.calc_symmetry(indices))
-                    # kumar index
-                    tau, l_e = index.kumar(distances, res=0.01)
-                    idx_kummar.append(tau)
-                    # concentration & contrast
-                    idx_concentration.append(index.concentration(distances))
-                    idx_contrast.append(index.relative_contrast_imp(distances))
-                    valid_epochs.append(e)
-                except:
-                    print("Epoch {} - no calculated embedding".format(e))
-            valid_epochs = np.array(valid_epochs)
-            idx_hubness = np.array(list(zip(*idx_hubness)))
-            idx_symmetry = np.array(list(zip(*idx_symmetry)))
-            idx_concentration = np.array(list(zip(*idx_concentration)))
-            idx_contrast = np.array(list(zip(*idx_contrast)))
-            idx_kummar = np.array(idx_kummar)
-            pickle.dump( (valid_epochs, idx_hubness, idx_symmetry, idx_concentration, idx_contrast, idx_kummar),
+            for i, source in enumerate(embed_source):
+                embd, epoch_mask = Ret.load_embedding(source, multi_epcch=True)
+
+                for e in epochs:
+                    try:
+                        Ret.fit(metric=metric, epoch=e)
+                        indices, distances = Ret.ret_nbrs()
+                        # hubness
+                        idx_hubness[i].append(index.calc_hubness(indices))
+                        #   symmetry
+                        idx_symmetry[i].append(index.calc_symmetry(indices))
+                        # kumar index
+                        tau, l_e = index.kumar(distances, res=0.01)
+                        idx_kummar[i].append(tau)
+                        # concentration & contrast
+                        idx_concentration[i].append(index.concentration(distances))
+                        idx_contrast[i].append(index.relative_contrast_imp(distances))
+                        valid_epochs[i].append(e)
+                    except:
+                        print("Epoch {} - no calculated embedding".format(e))
+                valid_epochs[i] = np.array(valid_epochs[i])
+                idx_hubness[i] = np.array(list(zip(*idx_hubness[i])))
+                idx_symmetry[i] = np.array(list(zip(*idx_symmetry[i])))
+                idx_concentration[i] = np.array(list(zip(*idx_concentration[i])))
+                idx_contrast[i] = np.array(list(zip(*idx_contrast[i])))
+                idx_kummar[i] = np.array([idx_kummar[i]])
+
+            combined_epochs = [i for i, c in enumerate(np.bincount(np.concatenate(valid_epochs))) if c > 3]
+
+            idx_hubness = mean_cross_validated_index(idx_hubness, valid_epochs, combined_epochs)
+            idx_symmetry = mean_cross_validated_index(idx_symmetry, valid_epochs, combined_epochs)
+            idx_concentration = mean_cross_validated_index(idx_concentration, valid_epochs, combined_epochs)
+            idx_contrast = mean_cross_validated_index(idx_contrast, valid_epochs, combined_epochs)
+            idx_kummar = mean_cross_validated_index(idx_kummar, valid_epochs, combined_epochs)
+
+            #idx_hubness = np.mean(idx_hubness, axis=0)
+            #idx_symmetry = np.mean(idx_symmetry, axis=0)
+            #idx_concentration = np.mean(idx_concentration, axis=0)
+            #idx_contrast = np.mean(idx_contrast, axis=0)
+            #idx_kummar = np.mean(idx_kummar, axis=0)
+            pickle.dump( (combined_epochs, idx_hubness, idx_symmetry, idx_concentration, idx_contrast, idx_kummar),
                          open(plot_data_filename, 'bw'))
         Epochs += [valid_epochs]
         Idx_hubness += [idx_hubness]
@@ -100,7 +137,7 @@ for epochs, idx_hubness, idx_symmetry, idx_concentration, idx_contrast, idx_kumm
         p[5 * m + 3].plot(epochs, smooth(idx_concentration[0] + idx_concentration[1]), color=q[0].get_color(), ls='--', alpha=alpha)
         p[5 * m + 3].plot(epochs, smooth(idx_concentration[0] - idx_concentration[1]), color=q[0].get_color(), ls='--', alpha=alpha)
         #   kumar
-        p[5 * m + 4].plot(epochs, smooth(idx_kummar))
+        p[5 * m + 4].plot(epochs, smooth(idx_kummar[0]))
         # labels
         if r == 0:  # first column
             p[5 * m + 0].axes.yaxis.label.set_text(metric)
